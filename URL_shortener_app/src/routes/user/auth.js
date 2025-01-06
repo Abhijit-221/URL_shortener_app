47ad56f239da2ab7a015f062a74476594238519b
Abhijit <swain.abhi2021@gmail.com>
2025-01-06 15:45:29 +0530

    modify app

--- URL_shortener_app/src/routes/user/auth.js
+++ URL_shortener_app/src/routes/user/auth.js
@@ -2,7 +2,7 @@ const express = require('express');
 const authController = require('../../controllers/user/auth');
 const userRouter = express.Router();
 
-userRouter.get('/login',authController.login);
+userRouter.get('/',authController.login);
 userRouter.get('/google/callback',authController.redirect);
 
 module.exports = userRouter;
